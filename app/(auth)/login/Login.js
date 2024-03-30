3bf19b69b8889b30c50f1c9a11432548bf3549c6
Donald646 <donaldchu901@gmail.com>
2024-03-29 23:20:38 -0500

    first init

---
+++ app/(auth)/login/Login.js
@@ -0,0 +1,12 @@
+export default function LoginPage() {
+  return (
+    <form>
+      <label htmlFor="email">Email:</label>
+      <input id="email" name="email" type="email" required />
+      <label htmlFor="password">Password:</label>
+      <input id="password" name="password" type="password" required />
+      <button>Log in</button>
+      <button>Sign up</button>
+    </form>
+  );
+}
